8cd780e67680492ab56782ce917cef8a137c405e
thebabellibrarybot <jack@thebabel.org>
2022-10-04 11:47:01 -0400

    removed frontend

--- client/src/api/axiosPrivate.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const axiosPrivate = axios.create({
-    // do i need a base url??
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
-})
