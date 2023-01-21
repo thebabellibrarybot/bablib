e605e098395f29789f6b49bc0c63d7d1da5fe4ee
thebabellibrarybot <jack@thebabel.org>
2023-01-21 13:15:11 -0500

    for heroku

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,8 +3,8 @@ const allowedOrigins = [
     'https://localhost:4000',
     'http://localhost:4000/',
     'http://127.0.0.1:5500',
-    'https://babellib.herokuapp.com/',
-    'https://thebabellibrary.herokuapp.com/'
+    'https://thebabellibrary.herokuapp.com/',
+    'https://thebabellibrary.herokuapp.com'
 ];
 
 module.exports = allowedOrigins
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -7,7 +7,7 @@ const corsOptions = {
         if (allowedOrgins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
-            callback(new Error('Not allowed by CORS'))
+            callback(new Error('Not allowed by CORS', allowedOrgins))
         }
     },
     optionsSuccessStatus: 200
