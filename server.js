e5e3f576226d760c145d81ab7d14c2555496a561
thebabellibrarybot <jack@thebabellibrary.org>
2023-02-06 14:10:15 -0500

    server

--- server.js
+++ server.js
@@ -86,14 +86,13 @@ app.use('/userspine', babelUserhomeRoute);
 
 
 // serve static assests if in production
-if(process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static('client/build'))
+app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 // port and clear run statements;
 app.listen(Port, () => {
     console.log(`listening on ${Port}`)
